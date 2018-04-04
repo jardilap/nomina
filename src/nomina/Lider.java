99010d779d8f9ef0806e772012b8d49449c6ad65
ESTUDIANTE <ESTUDIANTE@DESKTOP-KTO51U0>
2018-04-04 08:52:33 -0500

    Ejercicios clase 21 /02

---
+++ src/nomina/Lider.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nomina;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Lider extends Programadores{
+    private Programadores[] programador;
+    
+    public Lider(double salario, int ID, String lenguaje, String nombre) {
+        super(salario, ID, lenguaje, nombre);
+    }
+    
+    public void addPersonasEquipo(Programadores programador){
+        
+    }
+}
